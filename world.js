ad82492c09b5dde71bb8ec0ba067c41716d61ac6
caiofreitass <caiosilvaben3@gmail.com>
2026-03-09 23:51:31 -0300

    Update world.js

--- world.js
+++ world.js
@@ -2,12 +2,12 @@ const btnWorld = document.getElementById("btnWorld")
 const worldScreen = document.getElementById("world")
 
 let canvas
+let canvas
 let ctx
 
 let player = {
-x:100,
-y:100,
-speed:10
+x:200,
+y:200
 }
 
 function startWorld(){
@@ -15,35 +15,34 @@ function startWorld(){
 canvas = document.getElementById("canvas")
 ctx = canvas.getContext("2d")
 
+document.addEventListener("keydown", move)
+
 gameLoop()
+
+}
+
+function move(e){
+
+if(e.key=="w") player.y -= 5
+if(e.key=="s") player.y += 5
+if(e.key=="a") player.x -= 5
+if(e.key=="d") player.x += 5
+
+}
+
+function draw(){
+
+ctx.fillStyle = "#2e7d32"
+ctx.fillRect(0,0,canvas.width,canvas.height)
+
+ctx.fillStyle = "red"
+ctx.fillRect(player.x,player.y,20,20)
 
 }
 
 function gameLoop(){
 
 draw()
-
 requestAnimationFrame(gameLoop)
 
 }
-
-function draw(){
-
-ctx.fillStyle="#2e7d32"
-ctx.fillRect(0,0,canvas.width,canvas.height)
-
-ctx.fillStyle="red"
-ctx.fillRect(player.x,player.y,20,20)
-
-}
-
-document.addEventListener("keydown",(e)=>{
-
-if(e.key=="w") player.y-=player.speed
-if(e.key=="s") player.y+=player.speed
-if(e.key=="a") player.x-=player.speed
-if(e.key=="d") player.x+=player.speed
-
-draw()
-
-})
