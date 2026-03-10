f8466529410337a330c4535879d34e545a01c397
caiofreitass <caiosilvaben3@gmail.com>
2026-03-09 23:14:30 -0300

    Update world.js

--- world.js
+++ world.js
@@ -2,9 +2,14 @@ const btnWorld = document.getElementById("btnWorld")
 const worldScreen = document.getElementById("world")
 
 let canvas
+let canvas
 let ctx
 
-let player = {x:100,y:100}
+let player = {
+x:100,
+y:100,
+speed:10
+}
 
 function startWorld(){
 
@@ -25,14 +30,13 @@ ctx.fillRect(player.x,player.y,20,20)
 
 }
 
-document.addEventListener("keydown",e=>{
+document.addEventListener("keydown",(e)=>{
 
-if(e.key=="w") player.y-=10
-if(e.key=="s") player.y+=10
-if(e.key=="a") player.x-=10
-if(e.key=="d") player.x+=10
+if(e.key=="w") player.y-=player.speed
+if(e.key=="s") player.y+=player.speed
+if(e.key=="a") player.x-=player.speed
+if(e.key=="d") player.x+=player.speed
 
 draw()
-
 
 })
