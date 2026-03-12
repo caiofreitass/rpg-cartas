7acaa6af6fdba9c92c48baec8d031bb59c1426c4
caiofreitass <caiosilvaben3@gmail.com>
2026-03-12 13:49:18 -0300

    0.21

---
+++ public/world.js
@@ -0,0 +1,26 @@
+let canvas = document.getElementById("worldCanvas")
+let ctx = canvas.getContext("2d")
+
+let player = {x:200,y:200}
+
+document.addEventListener("keydown", e => {
+
+if(e.key=="w") player.y-=5
+if(e.key=="s") player.y+=5
+if(e.key=="a") player.x-=5
+if(e.key=="d") player.x+=5
+
+})
+
+function loop(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height)
+
+ctx.fillStyle="cyan"
+ctx.fillRect(player.x,player.y,20,20)
+
+requestAnimationFrame(loop)
+
+}
+
+loop()
